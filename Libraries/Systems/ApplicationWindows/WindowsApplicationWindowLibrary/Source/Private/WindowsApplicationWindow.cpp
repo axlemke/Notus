bd91ec8cc3f36773782c80c0b0267daa46c48f60
AxleMike <alexlemke@live.com>
2022-08-01 11:08:58 -0700

    More window setup

--- Libraries/Systems/ApplicationWindows/WindowsApplicationWindowLibrary/Source/Private/WindowsApplicationWindow.cpp
+++ Libraries/Systems/ApplicationWindows/WindowsApplicationWindowLibrary/Source/Private/WindowsApplicationWindow.cpp
@@ -2,3 +2,27 @@
 
 using namespace Systems;
 
+WindowsApplicationWindow::WindowsApplicationWindow(const char* name)
+	: ApplicationWindow(name)
+	, m_hInstance()
+	, m_windowClassEx()
+	, m_windowHandle()
+{
+}
+
+WindowsApplicationWindow::WindowsApplicationWindow()
+	: ApplicationWindow()
+	, m_hInstance()
+	, m_windowClassEx()
+	, m_windowHandle()
+{
+}
+
+WindowsApplicationWindow::~WindowsApplicationWindow()
+{
+}
+
+HWND WindowsApplicationWindow::GetWindowHandle() const
+{
+	return m_windowHandle;
+}
--- Libraries/Systems/ApplicationWindows/WindowsApplicationWindowLibrary/Source/Public/WindowsApplicationWindow.h
+++ Libraries/Systems/ApplicationWindows/WindowsApplicationWindowLibrary/Source/Public/WindowsApplicationWindow.h
@@ -2,12 +2,23 @@
 
 #include "Systems/ApplicationWindows/WindowsApplicationWindowLibrary/Source/Public/WindowsApplicationWindowCommon.h"
 #include <Systems/ApplicationWindows/ApplicationWindowInterfaceLibrary/Source/Public/ApplicationWindow.h>
+#include <Windows.h>
 
 namespace Systems
 {
 	class WindowsApplicationWindow : public ApplicationWindow
 	{
 	public:
+		WINDOWS_APPLICATION_WINDOW_USAGE WindowsApplicationWindow(const char* name);
+		WINDOWS_APPLICATION_WINDOW_USAGE WindowsApplicationWindow();
+		WINDOWS_APPLICATION_WINDOW_USAGE virtual ~WindowsApplicationWindow();
+
+		WINDOWS_APPLICATION_WINDOW_USAGE HWND GetWindowHandle() const;
+
+	protected:
+		HINSTANCE		m_hInstance;
+		WNDCLASSEX		m_windowClassEx;
+		HWND			m_windowHandle;
 
 	};
 }
