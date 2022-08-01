b4221e18a16a9b38996172825d98fdc050b37d50
AxleMike <alexlemke@live.com>
2022-08-01 12:11:40 -0700

    More setup

--- Libraries/Graphics/APIs/DirectXGraphicsLibrary/Source/Public/DirectXAPI.h
+++ Libraries/Graphics/APIs/DirectXGraphicsLibrary/Source/Public/DirectXAPI.h
@@ -3,3 +3,10 @@
 #include "Graphics\APIs\DirectXGraphicsLibrary\Source\Public\DirectXAPI.h"
 #include "Graphics\APIs\GraphicsInterfaceLibrary\Source\Public\GraphicsAPI\GraphicsAPI.h"
 
+namespace Graphics
+{
+	class DirectXAPI : public GraphicsAPI
+	{
+
+	};
+}
--- Libraries/Graphics/APIs/VulkanGraphicsLibrary/Source/Public/VulkanAPI.h
+++ Libraries/Graphics/APIs/VulkanGraphicsLibrary/Source/Public/VulkanAPI.h
@@ -3,4 +3,10 @@
 #include "Graphics\APIs\VulkanGraphicsLibrary\Source\Public\VulkanGraphicsCommon.h"
 #include "Graphics\APIs\GraphicsInterfaceLibrary\Source\Public\GraphicsAPI\GraphicsAPI.h"
 
+namespace Graphics
+{
+	class VulkanAPI : public GraphicsAPI
+	{
 
+	};
+}
