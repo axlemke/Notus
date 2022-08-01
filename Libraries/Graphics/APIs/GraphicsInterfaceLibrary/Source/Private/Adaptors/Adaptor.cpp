8a8e9ce99160fce56be50577e0155efb0f1fbd80
AxleMike <alexlemke@live.com>
2022-07-31 17:19:23 -0700

    More quick setup

--- Libraries/Graphics/APIs/GraphicsInterfaceLibrary/Source/Private/Adaptors/Adaptor.cpp
+++
@@ -1,3 +0,0 @@
-#include "Graphics\APIs\GraphicsInterfaceLibrary\Source\Public\Adaptors\Adaptor.h"
-
-
---
+++ Libraries/Graphics/APIs/GraphicsInterfaceLibrary/Source/Private/Adaptors/AdaptorInterface.cpp
@@ -0,0 +1,11 @@
+#include "Graphics/APIs/GraphicsInterfaceLibrary/Source/Public/Adaptors/AdaptorInterface.h"
+
+using namespace Graphics;
+
+AdaptorInterface::AdaptorInterface()
+{
+}
+
+AdaptorInterface::~AdaptorInterface()
+{
+}
